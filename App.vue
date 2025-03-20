b9284652f3d7d8f3f7fb96157632308bae2ec633
Mrwhite233 <970730966@qq.com>
2025-03-21 04:36:33 +0800

    2.封装请求

--- App.vue
+++ App.vue
@@ -2,19 +2,19 @@
 	export default {
 		// 相当于小程序app.js
 		// 这里负责监听整个程序的全局生命周期
-		onLaunch: function() {
-			console.log('App Launch, 全局App加载周期')
+		onLaunch: function () {
+			console.log('App Launch, 全局App加载周期1')
 		},
-		onShow: function() {
+		onShow: function () {
 			console.log('App Show, 全局App显示周期')
 		},
-		onHide: function() {
+		onHide: function () {
 			console.log('App Hide, 全局App隐藏周期')
 		}
 	}
 </script>
 <!-- app.vue中不能写template！！只能有script 和 style！！ -->
-<style lang='scss'>
+<style lang="scss">
 	/*每个页面公共css，即全局css全部写在这里 */
 	@import './fonts.scss';
 
